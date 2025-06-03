b1585ca47f3d7f21044cb3ebf9d912c11fc92306
Glenn Manguiat <manguiatgkd@aub.com.ph>
2025-06-03 11:53:55 +0800

    Initial Commit

---
+++ ScrumTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScrumTime
+//
+//  Created by Glenn Karlo Manguiat on 6/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScrumTime/ScrumTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumTimeApp.swift
+//  ScrumTime
+//
+//  Created by Glenn Karlo Manguiat on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
